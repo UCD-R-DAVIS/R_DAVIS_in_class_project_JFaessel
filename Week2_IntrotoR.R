4dd8caa2cd44d67314b30f829f12b8de1b274cb6
Junna Faessel <junnafaessel@campus-039-194.ucdavis.edu>
2024-10-03 14:23:42 -0700

    Week 2 Activity Version 2

--- Week2_IntrotoR.R
+++ Week2_IntrotoR.R
@@ -1,7 +1,77 @@
 #Week 2: Intro to R
 #Run commands with ctrl+enter. 
 #You can access help files with ?log
-#Errors halt a task, Warnings will attempt to finish the task
+#Other places to check out:
+#Google https://www.google.com/
+#Stack Overflow https://stackoverflow.com/
+#RStudio cheat sheets http://www.rstudio.com/resources/cheatsheets/
+#Cookbook for R http://www.cookbook-r.com/
+
+
+#Introduction to R: arithmetic
+3 + 4
+
+#incomplete command
+2 * 
+
+#order of operations
+4 + (8 * 3) ^ 2
+
+#scientific notation
+2 / 100000
+4e3
+
+#mathematical functions
+exp(1)
+exp(3)
+log(4)
+sqrt(4)
+
+#arguments to functions
+log(2, 4)
+log(4, 2)
+log(base = 4, x = 2)
+
+#x is still 1!
+x <- 1
+log(base = 4, x = 2)
+x
+#x is a confusing name. let's get rid of this
+rm(x)
+
+#nested functions
+sqrt(exp(4))
+log(exp(100))
+
+#six comparison functions
+mynumber <- 6
+#two equal signs!!
+mynumber == 5
+mynumber != 5
+mynumber > 4
+mynumber < 3
+mynumber >= 2
+mynumber <= -1
+
+#Objects and Assignment
+mynumber <- 7
+othernumber <- -3
+mynumber * othernumber
+
+#Object Name Conventions
+numSamples <- 50
+num_samples <- 40
+rm(num_samples)
+ls()
+
+#Errors and Warnings
+#NOTE: errors halt a task, warnings will attempt to finish the task
+log_of_word <- log("a_word")
+log_of_word
+
+log_of_negative <- log(-2)
+log_of_negative
+
 
 #Challenge
 elephant1_kg <- 3492
